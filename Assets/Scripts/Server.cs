51daae6600f1e921e6c44b729d7ad10671eb1653
Seok-Min-Lee <57294634+Seok-Min-Lee@users.noreply.github.com>
2025-08-29 17:56:51 +0900

    서버 및 DB에 데이터 전송 테스트

--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -56,18 +56,65 @@ public class Server : MonoBehaviour
     }
     public void ReceiveMessage(int connectionId, ArraySegment<byte> message)
     {
-        string sMessage = Encoding.UTF8.GetString(message);
-        Debug.Log(connectionId + "/" + sMessage);
+        // clear previous message
+        byte[] messageBytes = new byte[message.Count];
+        for (int i = 0; i < messageBytes.Length; i++)
+        {
+            messageBytes[i] = message.Array[i];
+        }
 
-        //// clear previous message
-        //byte[] messageBytes = new byte[message.Count];
-        //for (int i = 0; i < messageBytes.Length; i++)
-        //{
-        //    messageBytes[i] = message.Array[i];
-        //}
+        byte[] commandBytes = new byte[4];
+        Array.Copy(messageBytes, 0, commandBytes, 0, 4);
+        int command = BitConverter.ToInt32(commandBytes);
 
-        //byte[] commandBytes = new byte[4];
-        //Array.Copy(messageBytes, 0, commandBytes, 0, 4);
-        //int command = BitConverter.ToInt32(commandBytes);
+        switch (command)
+        {
+            case 1000:
+                ResponsePassword();
+                break;
+            case 1002:
+                ReceiveStudioData(ref messageBytes);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void ResponsePassword()
+    {
+        byte[] messages = new byte[8];
+
+        int command = 1001;
+        byte[] commandBytes = BitConverter.GetBytes(command);
+        Array.Copy(commandBytes, 0, messages, 0, 4);
+
+        int password = UnityEngine.Random.Range(0, 10000);
+        byte[] passwordBytes = BitConverter.GetBytes(password);
+        Array.Copy(passwordBytes, 0, messages, 4, 4);
+
+        server.Send(1, messages);
+
+        Debug.Log("ResponsePassword");
+    }
+
+    public void ReceiveStudioData(ref byte[] message)
+    {
+        byte[] passwordBytes = new byte[4];
+        Array.Copy(message, 4, passwordBytes, 0, 4);
+        int password = BitConverter.ToInt32(passwordBytes);
+
+        byte[] lengthBytes = new byte[4];
+        Array.Copy(message, 8, lengthBytes, 0, 4);
+        int length = BitConverter.ToInt32(lengthBytes);
+
+        byte[] textureByte = new byte[length];
+        Array.Copy(message, 12, textureByte, 0, length);
+
+        StudioDataSample sds = new StudioDataSample();
+        sds.id = password;
+        sds.textureRaw = textureByte;
+        GameObject.Find("db").GetComponent<DatabaseManager>().AddStudioData(sds, out string sResult);
+
+        Debug.Log("ReceiveStudioData");
     }
 }
